d8151d3110b183163357cbe617fda2b4dfaee03e
Nikhil Chand <nikhilchand000gkp.nc@gmail.com>
2025-08-22 11:55:53 +0530

    Day3

--- projects/project1.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to Band Name Generator.")
-city_name = input("What is the name of city you grew up in?\n")
-pet_name = input("What is your pet name?\n")
-print("Your band name could be: " + city_name + " " + pet_name)
